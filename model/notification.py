4805119aeda39bce47837fe6d22853fd14972592
OrionVi1998 <15966899+OrionVi1998@users.noreply.github.com>
2024-05-08 17:19:37 +0200

    Add comments to notification.py

--- model/notification.py
+++ model/notification.py
@@ -6,16 +6,16 @@ from utils.mongo_store_broker import mongo
 from bson.objectid import ObjectId
 
 
-@dataclass
+@dataclass  # Autogenerates __init__, __eq___ and __repr__ from given fields
 class Notification:
     text: str
     user_id: ObjectId
     _id: Optional[ObjectId] = ObjectId()
 
-    def get_id(self):
+    def get_id(self):  # in python _ before a field indicates that its protected so a method is used to retrive it
         return self._id
 
-    def __iter__(self):
+    def __iter__(self):  # This method allows us to add support for dict() on an object
         for field in dataclasses.fields(self):
             yield field.name, getattr(self, field.name)
 
@@ -24,6 +24,11 @@ class Dao:
 
     @staticmethod
     def find_one(_id: str | ObjectId):
+        """
+        Returns a notification object from db
+        :param _id:
+        :return:
+        """
         if isinstance(_id, str):
             _id = ObjectId(_id)
         res = mongo.db.notifications.find_one({"_id": _id})
@@ -31,10 +36,21 @@ class Dao:
 
     @staticmethod
     def insert_one(obj: dict | Notification):
+        """
+        Inserts one notification object into the db
+        :param obj:
+        :return:
+        """
         return mongo.db.notifications.insert_one(dict(obj)).inserted_id
 
     @staticmethod
     def update_one(_id: str | ObjectId, obj: Notification):
+        """
+        Updates a notification object inside the db
+        :param _id:
+        :param obj:
+        :return:
+        """
         if isinstance(_id, str):
             _id = ObjectId(_id)
         return mongo.db.notifications.update_one({"_id": _id},
@@ -42,11 +58,19 @@ class Dao:
 
     @staticmethod
     def delete_one(_id: str | ObjectId):
+        """
+        Deletes a notification object inside the db
+        :param _id:
+        :return:
+        """
         if isinstance(_id, str):
             _id = ObjectId(_id)
         return mongo.db.notifications.delete_one({"_id": _id})
 
     @staticmethod
     def get_all():
+        """
+        :return: - A list of Notification objects
+        """
         res = mongo.db.notifications.find({})
         return [Notification(**r) for r in res]
