0ba07a17f02bd0eff0123fe6941bd7b63ccd3cd6
OrionVi1998 <15966899+OrionVi1998@users.noreply.github.com>
2024-05-09 22:55:09 +0200

    Refactor, remove the unnecessary dao objects

--- model/user.py
+++ model/user.py
@@ -25,57 +25,54 @@ class User:
             yield field.name, getattr(self, field.name)
 
 
-class Dao:
+def find_one(_id: str | ObjectId):
+    """
+    Returns a user object from db
+    :param _id:
+    :return:
+    """
+    if isinstance(_id, str):
+        _id = ObjectId(_id)
+    res = mongo.db.users.find_one({"_id": _id})
+    return User(**res)
 
-    @staticmethod
-    def find_one(_id: str | ObjectId):
-        """
-        Returns a user object from db
-        :param _id:
-        :return:
-        """
-        if isinstance(_id, str):
-            _id = ObjectId(_id)
-        res = mongo.db.users.find_one({"_id": _id})
-        return User(**res)
 
-    @staticmethod
-    def insert_one(obj: dict | User):
-        """
-        Inserts one user object into the db
-        :param obj:
-        :return:
-        """
-        return mongo.db.users.insert_one(dict(obj)).inserted_id
+def insert_one(obj: dict | User):
+    """
+    Inserts one user object into the db
+    :param obj:
+    :return:
+    """
+    return mongo.db.users.insert_one(dict(obj)).inserted_id
 
-    @staticmethod
-    def update_one(_id: str | ObjectId, obj: User):
-        """
-        Updates a user object inside the db
-        :param _id:
-        :param obj:
-        :return:
-        """
-        if isinstance(_id, str):
-            _id = ObjectId(_id)
-        return mongo.db.users.update_one({"_id": _id},
-                                         {"$set": dict(obj)})
 
-    @staticmethod
-    def delete_one(_id: str | ObjectId):
-        """
-        Deletes a user object inside the db
-        :param _id:
-        :return:
-        """
-        if isinstance(_id, str):
-            _id = ObjectId(_id)
-        return mongo.db.users.delete_one({"_id": _id})
+def update_one(_id: str | ObjectId, obj: User):
+    """
+    Updates a user object inside the db
+    :param _id:
+    :param obj:
+    :return:
+    """
+    if isinstance(_id, str):
+        _id = ObjectId(_id)
+    return mongo.db.users.update_one({"_id": _id},
+                                     {"$set": dict(obj)})
 
-    @staticmethod
-    def get_all():
-        """
-        :return: - A list of user objects
-        """
-        res = mongo.db.users.find({})
-        return [User(**r) for r in res]
+
+def delete_one(_id: str | ObjectId):
+    """
+    Deletes a user object inside the db
+    :param _id:
+    :return:
+    """
+    if isinstance(_id, str):
+        _id = ObjectId(_id)
+    return mongo.db.users.delete_one({"_id": _id})
+
+
+def get_all():
+    """
+    :return: - A list of user objects
+    """
+    res = mongo.db.users.find({})
+    return [User(**r) for r in res]
