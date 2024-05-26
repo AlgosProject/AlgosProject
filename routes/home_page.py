e188e02db9cf047dd60411970abdd361ee6446c9
margotrichez <margot.richezfr@gmail.com>
2024-05-26 19:42:43 +0200

    fix like and dislike

--- routes/home_page.py
+++ routes/home_page.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, session, request
+from flask import Blueprint, render_template, session, request, redirect, url_for
 
 from model import userDao, postDao
 
@@ -7,10 +7,24 @@ home_blueprint = Blueprint("home_page", __name__, template_folder="templates")
 
 @home_blueprint.route("/home_page", methods=["GET", "POST"])
 def home():
-    # Get the user from session
-    user = userDao.User(**session.get("user"))
+    if request.method == "GET":
+        # Get the user from session
+        user = userDao.User(**session.get("user"))
+
+        # Get the users he can see
+        visible_user = user.bfs_get_visible_user_ids()
+
+        # Get the posts he can see and prepares them for the view
+        posts = []
+        for u_id in visible_user:
+            u_posts = postDao.find_posts_by_user(u_id)
+            for p in u_posts:
+                posts.append(p)
+
+        return render_template("home_page.jinja2", posts=posts, user=user)
 
     if request.method == "POST":
+        user = userDao.User(**session.get("user"))
         post_id = request.form.get("post_id")
         action = request.form.get("action")
         post = postDao.find_one(post_id)
@@ -37,14 +51,4 @@ def home():
                     dislikes.remove(user.id)
                     postDao.update_one(post_id, post)
 
-    # Get the users he can see
-    visible_user = user.bfs_get_visible_user_ids()
-
-    # Get the posts he can see and prepares them for the view
-    posts = []
-    for u_id in visible_user:
-        u_posts = postDao.find_posts_by_user(u_id)
-        for p in u_posts:
-            posts.append(p)
-
-    return render_template("home_page.jinja2", posts=posts, user=user)
+        return redirect(url_for("home_page.home"))
--- routes/profile_posts.py
+++ routes/profile_posts.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, session, request
+from bson import ObjectId
+from flask import Blueprint, render_template, session, request, redirect, url_for
 
 from model import userDao, postDao
 
@@ -8,30 +9,65 @@ profilePosts_blueprint = Blueprint("profile_posts", __name__, template_folder="t
 @profilePosts_blueprint.route("/profile_posts/<profile_id>", methods=['GET', 'POST'])
 @profilePosts_blueprint.route("/profile_posts", defaults={'profile_id': None}, methods=['GET', 'POST'])
 def profile_posts(profile_id=None):
-    curr_user = True
-    user = session.get('user')
-    current_user_id = user.get('_id')
-    profile = userDao.find_one(current_user_id)
+    if request.method == "GET":
+        curr_user = True
+        user = session.get('user')
+        current_user_id = user.get('_id')
+        profile = userDao.find_one(current_user_id)
 
-    user = userDao.User(**user)
-    friends_dict = user.get_friends_dict()
+        user = userDao.User(**user)
+        friends_dict = user.get_friends_dict()
 
-    friends_ls = [userDao.find_one(id) for id in friends_dict.keys()]
-    common_friends = dict()
+        friends_ls = [userDao.find_one(id) for id in friends_dict.keys()]
+        common_friends = dict()
 
-    for fr in friends_ls:
-        common_friends[fr.id] = len(set(fr.get_friends_dict().keys()).intersection(set(friends_dict.keys())))
+        for fr in friends_ls:
+            common_friends[fr.id] = len(set(fr.get_friends_dict().keys()).intersection(set(friends_dict.keys())))
 
-    if profile_id:
-        profile2 = userDao.find_one(profile_id)
-        curr_user = True if profile2 == profile else False
-        profile = profile2
+        if profile_id:
+            profile2 = userDao.find_one(profile_id)
+            curr_user = True if profile2 == profile else False
+            profile = profile2
 
-    u_posts = postDao.find_posts_by_user(profile.id)
-    posts = []
+        u_posts = postDao.find_posts_by_user(profile.id)
+        posts = []
 
-    for p in u_posts:
-        posts.append(p)
+        for p in u_posts:
+            posts.append(p)
 
-    return render_template("profile_posts.jinja2", profile=profile, posts=posts, curr_user=curr_user,
-                           common_friends=common_friends, user=user)
+        return render_template("profile_posts.jinja2", profile=profile, posts=posts,
+                               curr_user=curr_user, common_friends=common_friends, user=user)
+
+    if request.method == "POST":
+        user = userDao.User(**session.get("user"))
+        post_id = request.form.get("post_id")
+        action = request.form.get("action")
+        profile_id = request.form.get("profile_id")
+        post = postDao.find_one(post_id)
+        likes = post.likes
+        dislikes = post.dislikes
+
+        if action == "like_post" and post.user.id != user.id:
+            if user.id not in likes:
+                likes.append(user.id)
+                if user.id in dislikes:
+                    dislikes.remove(user.id)
+                postDao.update_one(post_id, post)
+            elif user.id in likes:
+                likes.remove(user.id)
+                postDao.update_one(post_id, post)
+
+        elif action == "dislike_post" and post.user.id != user.id:
+            if user.id not in dislikes:
+                dislikes.append(user.id)
+                if user.id in likes:
+                    likes.remove(user.id)
+                postDao.update_one(post_id, post)
+            elif user.id in dislikes:
+                dislikes.remove(user.id)
+                postDao.update_one(post_id, post)
+
+        if profile_id:
+            return redirect(url_for("profile_posts.profile_posts", profile_id=profile_id, _external=True))
+        else:
+            return redirect(url_for("profile_posts.profile_posts"))
