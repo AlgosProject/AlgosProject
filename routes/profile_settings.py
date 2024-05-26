f8ac93757ca16d6dfbe10c731ebdad60263bbee1
Aaron M <aaron.g.michael@gmail.com>
2024-05-26 19:36:45 +0200

    create endpoint

--- routes/profile_settings.py
+++ routes/profile_settings.py
@@ -1,8 +1,35 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session
+from model import userDao
 
 profileSettings_blueprint = Blueprint("profile_settings", __name__, template_folder="templates")
 
 
-@profileSettings_blueprint.route("/profile_settings")
+@profileSettings_blueprint.route("/profile_settings", methods=["GET", "POST"])
 def profile_settings():  # put application's code here
-    return render_template("profile_settings.jinja2")
+    if request.method == "GET":
+        return render_template("profile_settings.jinja2")
+
+    if request.method == "POST":
+        user = userDao.User(**session.get("user"))
+        calltype = request.form.get("update")
+        if calltype == "username":
+            username = request.form.get("username")
+            session['user'] = dict(user.set_username(username))
+        elif calltype == "password":
+            from flask import current_app
+            from flask_bcrypt import Bcrypt
+
+            bcrypt: Bcrypt = current_app.bcrypt
+            password = request.form.get("password")
+            password = bcrypt.generate_password_hash(password)
+            session['user'] = dict(user.set_password(password))
+        elif calltype == "name":
+            name = request.form.get("name")
+            session['user'] = dict(user.set_name(name))
+        elif calltype == "picture":
+            from utils import cloudinary_upload
+            picture = request.files["profile_picture"]
+            picture_url = cloudinary_upload.upload(picture)
+            session["user"] = dict(user.set_photo_url(picture_url))
+        flash("Updated " + calltype + " successfully")
+        return render_template("profile_settings.jinja2")
